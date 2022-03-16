c393d605494eea1484dd06c95c3a0165440ea605
mingwiki <mingwiki@gmail.com>
2022-03-16 17:40:45 +0800

    UPdate

---
+++ demo.js
@@ -0,0 +1,32 @@
+const http = require("http")
+const fs = require("fs")
+const { URL } = require("url")
+const serverAddr = "http://localhost"
+const Mock = require("mockjs")
+const { json } = require("stream/consumers")
+
+
+http.createServer((req, res) => {
+
+  const url = new URL(serverAddr + req.url)
+  switch (url.pathname) {
+    case "/getInfo":
+      let result = Mock.mock({
+        "姓名": "@cname",
+        "居住地": "@city",
+        "邮件": "@email",
+        "生日": '@date("yyyy-MM-dd")'
+      })
+      res.end(JSON.stringify(result))
+      break
+    default:
+      try {
+        let pathname = url.pathname === "/" ? "/demo.html" : url.pathname
+        res.end(fs.readFileSync(__dirname + pathname))
+      } catch (e) {
+        res.writeHead(404, "Not Found")
+        res.end(`<title>Hello demo</title><h1>404 Not Found</h1>`)
+      }
+  }
+}).listen(80)
+console.log(serverAddr)
